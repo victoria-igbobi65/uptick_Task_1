94537a88a16460258d8c28b6ef0ca7b2963192af
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-09-20 21:56:56 +0100

    new: middleware to authenticate visitor

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,22 @@
+const { StatusCodes } = require("http-status-codes");
+const { promisify } = require('util')
+const { AppError } = require("../errors/AppError");
+const CatchAsync = require("../errors/CatchAsync");
+const { promisify } = require("util");
+const { decodeToken } = require("../utils/helper");
+const { getUserById } = require("../repositories/auth");
+
+const protect = CatchAsync(async(req, res, next) => {
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return next(new AppError('You are not logged in!', StatusCodes.UNAUTHORIZED))
+    }
+
+    const decode = await decodeToken(token);
+    const currentUser = await getUserById(decode.id);
+    if (!currentUser) return next(new AppError('The user that has this token no longer exixts!'))
+})
